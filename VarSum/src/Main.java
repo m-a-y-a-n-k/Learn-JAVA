2e022ef654ed29340cb9b43ec92363575b139881
Mayank Narula <mnarulasubs@gmail.com>
2023-07-16 12:27:28 +0530

    Learn JAVA Module 2 Var Sum

---
+++ VarSum/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 2, num2 = 3, num3 = 5;
+        System.out.print("The sum of " + num1 + ", " + num2 + " and " + num3 + " is : ");
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
